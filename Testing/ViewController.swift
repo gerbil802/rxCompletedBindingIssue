2639aa3960aa478c6669f92e94428e5db89ed0fb
Tal Shrestha <talshrestha@gmail.com>
2017-08-02 15:34:10 +0300

    Initial Commit

---
+++ Testing/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Testing
+//
+//  Created by Tal Shrestha on 02/08/2017.
+//  Copyright © 2017 None. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
